fc179edd43ee22a487c3f5b49b5aae6ed669a19d
Mikaeel Khalid <mikaeelkhalid@icloud.com>
2024-06-08 16:45:09 +0200

    cleaning

---
+++ stacks/s3-bucket-policy-stack.ts
@@ -0,0 +1,10 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class S3BucketPolicyStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    
+  }
+}
